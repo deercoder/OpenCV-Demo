666cc5da8eb603d1dc388564f6a20d93e06a3d98
Liu Chang <lllccc2009@gmail.com>
2016-05-14 15:28:44 -0400

    add OpenCV native .mk file
    this is used for building native OpenCV code, as here:
    include ${OPENCVROOT}/sdk/native/jni/OpenCV.mk
    thus we may use the native libraries

--- app/src/main/jni/Android.mk
+++ app/src/main/jni/Android.mk
@@ -2,6 +2,14 @@ LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 
+#opencv
+OPENCVROOT:= /home/changliu/OpenCV-android-sdk
+OPENCV_CAMERA_MODULES:=on
+OPENCV_INSTALL_MODULES:=on
+OPENCV_LIB_TYPE:=SHARED
+include ${OPENCVROOT}/sdk/native/jni/OpenCV.mk
+
+
 LOCAL_SRC_FILES := hello-android-jni.c
 LOCAL_LDLIBS += -llog
 LOCAL_MODULE := hello
